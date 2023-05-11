97dea5b55740d6866bea0228b9b10265edd498e0
Daniel Schußmann <9323963+DanielSchussmann@users.noreply.github.com>
2023-05-11 04:54:27 -0400

    28/01/23 updatxxe

--- app.py
+++ app.py
@@ -8,25 +8,9 @@ from flask import render_template
 import jinja2
 from jinja2 import Template
 
-#template = jinja2.Template( source='templates/webpage/results_style_in_html.html')
-"""
-with open('templates/webpage/results_style_in_html.html.jinja2') as file_:
-    template = Template(file_.read())
-
-rendered = template.render(posts_in_tf=100, avg_likes=1000)
-
-file_name = 'html_saves/' + 'user_name' + ' ' + str(datetime.datetime.now())[:10] + '.html'
-save_html = open(file_name, "w")
-save_html.write(rendered)
-save_html.close()
-"""
-
-
 client = slack.WebClient(token='xoxb-5125654014036-5146891840544-2WGECylsIlXlq00fenhF5vKy')
 test_channel = 'C053PKBHDB6'
-
-#client.files_upload(channels=test_channel, initial_comment='user_name'+ "'s KPI",file=file_name)
-
+client.chat_postMessage(channel=test_channel, text='input formatting is false')
 
 
 result = client.conversations_history(channel=test_channel)
@@ -45,8 +29,7 @@ for msg in conversation_history:
             # Send result data to result_data HTML file
             com('\__Scrape initiated successfully')
             result = scarpe_and_stat(link + 'recent-activity/shares/')
-            # print(result)
-            # print([result['mip'].to_html(classes='data', header="true")])
+
             with open('templates/webpage/results_style_in_html.html.jinja2') as file_:
                 template = Template(file_.read())
 
@@ -69,7 +52,8 @@ for msg in conversation_history:
             save_html.write(render)
             save_html.close()
             client.files_upload(channels=test_channel, initial_comment=result['user_name'] + "'s KPI",thread_ts=msg['ts'],file=file_name)
-
+            driver.close()
+            time.sleep(10)
             #client.chat_postMessage(channel=test_channel, thread_ts=msg['ts'], text='HELLOU')
 
 
